23b09f8408bc6811b402685adc294ffec78dae5f
jan <jan@digitalboutique.co.uk>
2025-05-24 11:47:09 +0200

    Update PINs to RPi5 Adding more services for fully containerized solution

--- pin_linux.go
+++ pin_linux.go
@@ -4,8 +4,9 @@ import "github.com/davecheney/gpio"
 
 const (
 	irqPin = gpio.GPIO25
+	base   = 571
 )
 
 func getPin() (gpio.Pin, error) {
-	return gpio.OpenPin(irqPin, gpio.ModeInput)
+	return gpio.OpenPin(base+irqPin, gpio.ModeInput)
 }
